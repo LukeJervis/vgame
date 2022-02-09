ddb82c7cac744d9f7e7a230c5bc31be73a486829
Luke Jervis <88159203+viper14lk@users.noreply.github.com>
2022-02-09 21:52:51 +0000

    bug fix

--- src/components/PatrolLocations/PatrolBattle.js
+++ src/components/PatrolLocations/PatrolBattle.js
@@ -7,16 +7,6 @@ const PatrolBattle = (props) => {
     const { heroActionStore: { patrolBattleAttack, monsterHealth, monsterLevel, monsterName, underAttack, patrolBattleStart } } = useRootStore()
     const { heroStatsStore: { health } } = useRootStore()
 
-    const continuePatrol = () => {
-        if (underAttack) {
-            console.log('Still in battle')
-        } else if (health <= 0){
-            console.log('You is dead brah');
-        } else {
-            patrolBattleStart(props.location, props.num1, props.num2)
-        }
-    }
-
     return (
         <div className='patrolBattle__container' >
             <div className='PatrolBattle--attack' onClick={() => patrolBattleAttack()}>
@@ -34,8 +24,9 @@ const PatrolBattle = (props) => {
                         Health: {monsterHealth}
                     </div>
                 </div>
-                HIT
-                <button className='PatrolBattle__continueButton' onClick={continuePatrol}>Continue</button>
+                <div className='patrolBattle__attackArea'>
+                    Click box to attack
+                </div>
             </div>
         </div>
     )
--- src/layout/HeroStatus.js
+++ src/layout/HeroStatus.js
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import { useRootStore } from '../provider/RootStoreProvider'
+import './heroStatus.css'
 
 const HeroStatus = () => {
 
